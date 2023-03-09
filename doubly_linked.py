578a01b0def5bf99ef434f313ebcfb156f4ed9a1
Matthew Guizar <matthewrguizar@gmail.com>
2023-03-08 19:29:00 -0800

    added get method

--- doubly_linked.py
+++ doubly_linked.py
@@ -70,13 +70,22 @@ class DoublyLinkedList:
         self.length -= 1
         return temp
 
+    def get(self, index):
+        if index < 0 or index >= self.length:
+            return None
+        temp = self.head
+        if index < self.length / 2:
+            for _ in range(index):
+                temp = temp.next
+        else:
+            temp = self.tail
+            for _ in range(self.length - 1, index, -1):
+                temp = temp.prev
+        return temp
 
 
-doubly_linked_list = DoublyLinkedList(2)
+doubly_linked_list = DoublyLinkedList(0)
 
 doubly_linked_list.append(3)
 
-
-print(doubly_linked_list.pop_first())
-print(doubly_linked_list.pop_first())
-print(doubly_linked_list.pop_first())
+print(doubly_linked_list.get(1))
